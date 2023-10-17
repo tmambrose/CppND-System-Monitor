35c6f5522cd89a742f61c37966957889f5fcb557
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-10-17 02:24:27 +0000

    Built Format::ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iomanip>
 
 #include "format.h"
 
@@ -8,4 +9,17 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+// string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+
+string Format::ElapsedTime(long seconds) {
+    std::string hh, mm, ss;
+    ss = std::to_string(seconds % 60);
+    mm = std::to_string((seconds / 60) % 60);
+    hh = std::to_string(seconds / 3600);
+    string hms = hh.length() > 1 ? hh : "0" + hh;
+    hms += ":";
+    hms += mm.length() > 1 ? mm : "0" + mm;
+    hms += ":";
+    hms += ss.length() > 1 ? ss : "0" + ss;
+    return hms; 
+}
