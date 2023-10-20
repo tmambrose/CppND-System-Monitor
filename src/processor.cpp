74579c7964e82937fa199ecaffe8407a11bb616a
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-10-20 12:40:56 +0000

    Implement constructor, call to constructor

--- src/processor.cpp
+++ src/processor.cpp
@@ -8,22 +8,37 @@
 using std::vector;
 using std::string;
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() {
+// Initialize aggregate CPU processor
+// TODO (unrequired): specify CPU for multi-core to return
+// report for each core.
+Processor::Processor() {
     vector<string> cpu_stats = LinuxParser::CpuUtilization();
 
-    float kUser = stof(cpu_stats[LinuxParser::kUser_]);
-    float kNice = stof(cpu_stats[LinuxParser::kNice_]);
-    float kSystem = stof(cpu_stats[LinuxParser::kSystem_]);
-    float kIRQ = stof(cpu_stats[LinuxParser::kIRQ_]);
-    float kSoftIRQ = stof(cpu_stats[LinuxParser::kSoftIRQ_]);
-    float kSteal = stof(cpu_stats[LinuxParser::kSteal_]);
+    user_ = stof(cpu_stats[LinuxParser::kUser_]);
+    nice_ = stof(cpu_stats[LinuxParser::kNice_]);
+    system_ = stof(cpu_stats[LinuxParser::kSystem_]);
+    idle_ = stof(cpu_stats[LinuxParser::kIdle_]);
+    iowait_ = stof(cpu_stats[LinuxParser::kIOwait_]);
+    irq_ = stof(cpu_stats[LinuxParser::kIRQ_]);
+    softIrq_ = stof(cpu_stats[LinuxParser::kSoftIRQ_]);
+    steal_ = stof(cpu_stats[LinuxParser::kSteal_]);
+    guest_ = stof(cpu_stats[LinuxParser::kGuest_]);
+    guestNice_ = stof(cpu_stats[LinuxParser::kGuestNice_]);
+}
 
-    float kIdle = stof(cpu_stats[LinuxParser::kIdle_]);
-    float kIOwait = stof(cpu_stats[LinuxParser::kIOwait_]);
+// TODO: Return the aggregate CPU utilization
+/*
+    The amount of time, measured in units of USER_HZ
+    (1/100ths of a second on most architectures, use
+    sysconf(_SC_CLK_TCK) to obtain the right value),
+    that the system ("cpu" line) or the specific CPU
+    ("cpuN" line) spent in various states.
+*/
+float Processor::Utilization() {
+    Processor cpu = Processor();
 
-    float idle = kIdle + kIOwait;
-    float nonIdle = kUser + kNice + kSystem + kIRQ + kSoftIRQ + kSteal;
+    float idle = cpu.idle_ + cpu.iowait_;
+    float nonIdle = cpu.user_ + cpu.nice_ + cpu.system_ + cpu.irq_ + cpu.softIrq_ + cpu.steal_;
     float total = idle + nonIdle;
 
     float utilization = (total - idle) / total;
