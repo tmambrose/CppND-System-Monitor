250eced2afe765eaa1c90dafd800c15626a73c13
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-10-21 03:06:58 +0000

    Remove constructor override

--- include/processor.h
+++ include/processor.h
@@ -1,23 +1,28 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
 class Processor {
  public:
-  Processor();
   float Utilization();  // TODO: See src/processor.cpp
-
-  // Done: Declare any necessary private members
+ // DONE: Declare any necessary private members
  private:
-   float user_;  // Time (Hz) spent in user mode.
-   float nice_;  // Time (Hz) spent in user mode with low priority (nice).
-   float system_;  // Time (Hz) spent in system mode.
-   float idle_;  // Time (Hz) spent in the idle task.
-   float iowait_;  // iowait (Hz) (since Linux 2.5.41); Time waiting for I/O to complete (unreliable, see man pages).
-   float irq_;  // Time (Hz) servicing interrupts (since Linux 2.6.0).
-   float softIrq_;  // Time (Hz) servicing softirqs (since Linux 2.6.0).
-   float steal_;  // Stolen time (Hz), which is the time spent in other operating systems when running in a virtualized environment (since Linux 2.6.11).
-   float guest_;  // Time (Hz) spent running a virtual CPU for guest operating systems under the control of the Linux kernel (since Linux 2.6.24).
-   float guestNice_;  // Time (Hz) spent running a niced guest (virtual CPU for guest operating systems under the control of the Linux kernel) (since Linux 2.6.33).
+   vector<string> cpu_stats_ {};
+   float user_ {};  // Time (Hz) spent in user mode.
+   float nice_ {};  // Time (Hz) spent in user mode with low priority (nice).
+   float system_ {};  // Time (Hz) spent in system mode.
+   float idle_ {};  // Time (Hz) spent in the idle task.
+   float iowait_ {};  // iowait (Hz) (since Linux 2.5.41); Time waiting for I/O to complete (unreliable, see man pages).
+   float irq_ {};  // Time (Hz) servicing interrupts (since Linux 2.6.0).
+   float softIrq_ {};  // Time (Hz) servicing softirqs (since Linux 2.6.0).
+   float steal_ {};  // Stolen time (Hz), which is the time spent in other operating systems when running in a virtualized environment (since Linux 2.6.11).
+   float guest_ {};  // Time (Hz) spent running a virtual CPU for guest operating systems under the control of the Linux kernel (since Linux 2.6.24).
+   float guestNice_ {};  // Time (Hz) spent running a niced guest (virtual CPU for guest operating systems under the control of the Linux kernel) (since Linux 2.6.33).
 };
 
 #endif
