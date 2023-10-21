f102e9d00fc55880c289e835666c42051cbb195c
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-10-21 04:03:02 +0000

    Update TODO to DONE

--- src/system.cpp
+++ src/system.cpp
@@ -39,7 +39,7 @@ std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
 
-// TODO: Return the total number of processes on the system
+// DONE: Return the total number of processes on the system
 int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
 // TODO: Return the number of seconds since the system started running
