7e7c909ee6f7962551e179c8d15a48621a755dc3
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-10-29 18:54:47 +0000

    Add call to Command parser

--- src/process.cpp
+++ src/process.cpp
@@ -20,7 +20,7 @@ int Process::Pid() { return pid_; }
 float Process::CpuUtilization() { return 0; }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() { return LinuxParser::Command(pid_); }
 
 // DONE: Return this process's memory utilization
 string Process::Ram() { return LinuxParser::Ram(pid_); }
