90d2361cbe5c3080355e5486433a5aa49ae32f19
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-10-29 18:45:07 +0000

    Add call to Ram()

--- src/process.cpp
+++ src/process.cpp
@@ -22,13 +22,13 @@ float Process::CpuUtilization() { return 0; }
 // TODO: Return the command that generated this process
 string Process::Command() { return string(); }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// DONE: Return this process's memory utilization
+string Process::Ram() { return LinuxParser::Ram(pid_); }
 
-// TODO: Return the user (name) that generated this process
+// DONE: Return the user (name) that generated this process
 string Process::User() { return LinuxParser::User(pid_); }
 
-// TODO: Return the age of this process (in seconds)
+// DONE: Return the age of this process (in seconds)
 long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
